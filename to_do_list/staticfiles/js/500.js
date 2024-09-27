7c8baa9c98a2716b199e3572b520974b22fe6e4d
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-09-27 20:57:26 +0330

    Delete to_do_list/staticfiles directory

--- to_do_list/staticfiles/js/500.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function(){
-    document.body.classList.remove('loading');
-  }, 1000);
-});
-
-setTimeout(function() {
-    window.location.href = '../home';
-}, 120000);
